78c2a00e1275e3d8d19487d14434fef1512e2832
lvd536 <nbikiti@gmail.com>
2026-01-03 17:42:41 +0400

    - Edit genres page

--- app/anime/categories/page.tsx
+++ app/anime/categories/page.tsx
@@ -4,21 +4,22 @@ import Image from "next/image";
 export default async function page() {
     const genres = await getAllGenres();
     return (
-        <div className="grid grid-cols-1 xs:grid-cols-[repeat(2,200px)] sm:grid-cols-[repeat(5,1fr)] gap-5">
+        <div className="grid grid-cols-1 xs:grid-cols-[repeat(2,200px)] sm:grid-cols-[repeat(5,1fr)] gap-9">
             {genres.map((genre) => (
-                <div key={genre.id} className="relative">
+                <div
+                    key={genre.id}
+                    className="hover:ring-1 hover:ring-white rounded-t-sm transition-ring duration-300"
+                >
                     <Image
                         src={apiRoutes.image(genre.image.preview)}
                         alt=""
                         width={150}
                         height={250}
-                        className="w-full h-full"
+                        className="w-full h-full rounded-t-sm"
                     />
-                    <div className="absolute left-0 top-0 flex flex-col justify-between items-center opacity-0 hover:opacity-100 bg-black/60 w-full h-full transition-opacity duration-500 py-2">
-                        <h1 className="font-semibold text-xl text-center">
-                            {genre.name}
-                        </h1>
-                    </div>
+                    <p className="text-md font-semibold px-2 py-1 bg-gray-600 rounded-b-xl">
+                        {genre.name}
+                    </p>
                 </div>
             ))}
         </div>
