89cd15500cb5418493577cd1e8b1e9a9bd448382
lvd536 <nbikiti@gmail.com>
2026-01-05 00:17:53 +0400

    - Finish adaptive for home

--- components/Home/Lists/Latest/LatestList.tsx
+++ components/Home/Lists/Latest/LatestList.tsx
@@ -3,9 +3,14 @@ import AnimeCard from "@/components/AnimeCard/AnimeCard";
 export default async function LatestList() {
     const latestAnime = await getLatestReleases(6);
     return (
-        <ul className="grid grid-cols-3 xl:grid-cols-6 justify-between gap-4 mt-2">
-            {latestAnime.map((anime) => (
-                <AnimeCard key={anime.id} anime={anime} />
+        <ul className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-6 justify-between gap-4 mt-2">
+            {latestAnime.map((anime, index) => (
+                <li
+                    key={anime.id}
+                    className={index >= 3 ? "hidden sm:block" : "block"}
+                >
+                    <AnimeCard anime={anime} />
+                </li>
             ))}
         </ul>
     );
