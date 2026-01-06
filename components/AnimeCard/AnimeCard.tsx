359092f1d530e8521c05ef30dab6434fd30c8d0d
lvd536 <nbikiti@gmail.com>
2026-01-06 15:50:12 +0400

    - Fix anime card hover

--- components/AnimeCard/AnimeCard.tsx
+++ components/AnimeCard/AnimeCard.tsx
@@ -23,7 +23,7 @@ export default function AnimeCard({ anime }: IProps) {
                 width={1080}
                 className="w-50 h-72 rounded-lg object-cover"
             />
-            <div className="absolute left-0 top-0 flex flex-col justify-between items-center opacity-0 hover:opacity-100 bg-black/60 w-full h-full transition-opacity duration-500 py-2 rounded-lg">
+            <div className="absolute left-0 top-0 flex flex-col justify-between items-center opacity-0 hover:opacity-100 bg-black/60 w-50 h-72 transition-opacity duration-500 py-2 rounded-lg">
                 {"latest_episode" in anime && anime.latest_episode && (
                     <p>{anime.latest_episode.ordinal} Эпизод</p>
                 )}
