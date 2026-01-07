6439bf522dd5d7ba9faeee0bff108b84ce79c86a
lvd536 <nbikiti@gmail.com>
2026-01-07 22:10:57 +0400

    - Lazy anime loading & node for filters in releases

--- app/catalog/releases/page.tsx
+++ app/catalog/releases/page.tsx
@@ -1,13 +1,5 @@
 import Releases from "@/components/Catalog/Releases/Releases";
-import { getCatalog } from "@/helpers/api";
 
-interface IProps {
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}
-
-export default async function page({ searchParams }: IProps) {
-    const params = await searchParams;
-    const page: number = Number(params.page) || 1;
-    const animeCatalog = await getCatalog(page);
-    return <Releases releasesList={animeCatalog} />;
+export default function page() {
+    return <Releases />;
 }
--- components/Catalog/Releases/Releases.tsx
+++ components/Catalog/Releases/Releases.tsx
@@ -1,20 +1,65 @@
+"use client";
+import { useEffect, useState } from "react";
 import ReleaseCard from "./ReleaseCard";
 import { CatalogAnime, CatalogResponse } from "@/types/api.types";
+import { getCatalog } from "@/helpers/api";
 
-interface IProps {
-    releasesList: CatalogResponse<CatalogAnime>
-}
+export default function Releases() {
+    const [animePage, setCurrentPage] = useState<number>(1);
+    const [animeList, setAnimeList] =
+        useState<CatalogResponse<CatalogAnime> | null>(null);
+    const [fetching, setFetching] = useState<boolean>(true);
 
-export default async function Releases({ releasesList }: IProps) {
+    useEffect(() => {
+        if (fetching) {
+            getCatalog(animePage).then((data) => {
+                if (data) {
+                    setAnimeList((prev) => ({
+                        ...data,
+                        data: [...(prev?.data || []), ...data.data],
+                    }));
+                    setCurrentPage((prev) => prev + 1);
+                    setFetching(false);
+                }
+            });
+        }
+    }, [fetching, animePage]);
+
+    useEffect(() => {
+        function scrollHandler(e: Event) {
+            const target = e.target as Document;
+            const { documentElement } = target;
+            if (
+                documentElement.scrollHeight -
+                    (documentElement.scrollTop + window.innerHeight) <
+                100
+            ) {
+                setFetching(true);
+            }
+        }
+        document.addEventListener("scroll", scrollHandler);
+
+        return () => document.removeEventListener("scroll", scrollHandler);
+    }, []);
+
     return (
         <div className="flex gap-2 items-start justify-between">
-            <ul className="max-lg:w-1/2 w-7/10 flex flex-col justify-center gap-5 bg-stone-600/25 rounded-lg">
-                {releasesList.data.map((anime) => (
-                    <ReleaseCard release={anime} key={anime.id} />
-                ))}
-            </ul>
+            {animeList ? (
+                <>
+                    <ul className="max-lg:w-1/2 w-7/10 flex flex-col justify-center gap-5 bg-stone-600/25 rounded-lg">
+                        {animeList.data.map((anime) => (
+                            <ReleaseCard release={anime} key={anime.id} />
+                        ))}
+                    </ul>
+                    {fetching && <p>Fetching next results...</p>}
+                </>
+            ) : fetching ? (
+                <div>Loading anime list!</div>
+            ) : (
+                <div>Error</div>
+            )}
             <div className="max-lg:w-1/2 w-3/10 flex bg-stone-600/25 px-3 py-4 rounded-lg">
-                123
+                TODO: Search bar & filtering
             </div>
         </div>
     );
